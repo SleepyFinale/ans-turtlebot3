3d35cef7a6a5b8f5d80588199c05fa9f4b0b7675
SleepyFinale <SleepyFinale@proton.me>
2026-02-12 16:09:32 -0600

    Remove build_node_and_source.sh script as it is no longer needed for the build process, streamlining the project structure.

--- scripts/build_node_and_source.sh
+++
@@ -1,4 +0,0 @@
-echo "Building workspace (colcon build --packages-select turtlebot3_node --symlink-install --parallel-workers 1)..."
-colcon build --packages-select turtlebot3_node --symlink-install --parallel-workers 1
-echo "sourcing install/setup.bash"
-source install/setup.bash
